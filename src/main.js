a246767020bfa9f64062cc756420dc137b7183ad
Ashish singh <ashishsingh4857@gmail.com>
2025-09-19 00:31:35 +0530

    feat:added city name input  functionality

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
 import "./style.css";
+import { getWeatherData } from "./service/api.js";
+import { insertDataInHtml } from "./dom/dom.js";
+
+const inputCity = document.querySelector(".city_name");
+const weatherSearchForm = document.querySelector(".weather_search");
+let city = "jabalpur";
+// Handle city name search
+const cityNameSearching = async (e) => {
+	e.preventDefault();
+	city = inputCity.value.trim();
+	if (city) {
+		const apiData = await getWeatherData(city);
+
+		if (apiData) {
+			insertDataInHtml(apiData);
+		}
+	} else {
+		alert("Please enter a city name");
+	}
+	inputCity.value = "";
+};
+
+weatherSearchForm.addEventListener("submit", cityNameSearching);
